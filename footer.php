1e5c25e397dcf14a94523138b9c70dbf089a0241
DefunctLizard <grantshandy@gmail.com>
2019-12-01 16:55:40 -0700

    upload for bug fixes

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+<table width="100%" border="0" cellspacing="0">
+	<tr>
+		<td align="left"><a href="http://www.apache.org/"><img src="/apache_pb.gif" border="0" width="259" height="32" alt=Apache></a></td>
+		<td align="center"><a href="https://voidlinux.org"><img src="/void.png" alt="Void Linux" height="60" width="70"></a></td>
+		<td align="right"><a href="http://www.linux.org/"><img src="/tux.png" border="0" width="150" height="85" alt=Linux></a></td>
+	</tr>
+</table>
+?>
