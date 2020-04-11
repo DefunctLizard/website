54ebeddad785047ecebc65ee75ce480203187dac
DefunctLizard <grantshandy@gmail.com>
2020-04-11 06:07:04 +0000

    change icon to my profile!

--- custom_50x.php
+++ custom_50x.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
 	<meta charset="UTF-8">
-	<link rel="icon" href="/icon.svg">
+	<link rel="icon" href="/icon.ico">
 	<link rel='stylesheet' type='text/css' href='style.css'>
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
