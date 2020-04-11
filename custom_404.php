620015218dc41d003d416979b411a53b0c1ebdd1
DefunctLizard <grantshandy@gmail.com>
2020-04-11 06:01:58 +0000

    Finish?!

---
+++ custom_404.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+	<meta charset="UTF-8">
+	<link rel="icon" href="/icon.svg">
+	<link rel='stylesheet' type='text/css' href='style.css'>
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Grant Handy">
+        <meta name="description" content="404, page not found (idiot)">
+	<title>404 Page Not Found</title>
+	<link href="https://fonts.googleapis.com/css2?family=Overpass:ital@1&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="container">
+		<h1 align="center">404, That's an error (idiot)</h1>
+		<hr>
+		<p align="center">&copy; <?php echo date("Y"); ?></p>
+        </div>
+    </body>
+</html>
---
+++ custom_50x.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+    <head>
+	<meta charset="UTF-8">
+	<link rel="icon" href="/icon.svg">
+	<link rel='stylesheet' type='text/css' href='style.css'>
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Grant Handy">
+        <meta name="description" content="Main page Grant Handy's personal website.">
+	<title>Error: 50X</title>
+	<link href="https://fonts.googleapis.com/css2?family=Overpass:ital@1&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="container">
+		<h1 align="center">HEY! You just got a strange 50X error...</h1>
+		<p align="center">Please contact me at grantshandy@gmail.com</p>
+		<hr>
+		<p align="center">&copy; <?php echo date("Y"); ?></p>
+        </div>
+    </body>
+</html>
